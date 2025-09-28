cf21be7c14aab95252009ebcfb5b1a1ff7e7a936
Joao Romao <tupi_1800@hotmail.com>
2025-09-27 23:31:21 -0300

    playwrite test

--- estudo/Assertions.js
+++ estudo/Assertions.js
@@ -1,43 +1,32 @@
 const { chromium, expect } = require('@playwright/test');
 
-(async () => {
-    const browser = await chromium.launch()
-    const context = await browser.newContext()
-    const page = await context.newPage()
+test.describe('Assertions Example', () => {
+  test('should perform various assertions on the page', async ({ page }) => {
     await page.goto('https://react-redux.realworld.io/#/login')
 
     await page.fill('input[type = "email"]', 'alanvoigt@yahoo.com.br')
-    await page.press('input[type = "email"]', 'Tab')
     await page.type('input[type = "password"]', 'test123')
     await page.click('form >> "Sign in"')
 
-    await page.waitForTimeout(5000)
+    // É melhor esperar por um elemento específico do que usar um timeout fixo.
+    await page.waitForSelector('.feed-toggle');
 
     const logoText = await page.$eval('.navbar-brand', el => el.innerText)
     expect(logoText).toBe('conduit')
 
     const logoHref = await page.$eval('.navbar-brand', el => el.href)
-    expect(logoHref).toBeDefined()
+    expect(logoHref).toBe('https://react-redux.realworld.io/#/')
 
     const popularTagsCount = await page.$$eval('.tag-default', el => el.length)
     expect(popularTagsCount).toBeGreaterThanOrEqual(5)
-    expect(popularTagsCount).toBeLessThanOrEqual(30)
-    expect(popularTagsCount).toEqual(20)
-
 
     const content = await page.textContent('.navbar-brand')
-    // console.log('content: ' + content)
-
-    const text = await page.innerText('.navbar-brand')
-    // console.log('text: ' + text)
+    expect(content).toBe('conduit')
 
     const html = await page.innerHTML('.feed-toggle')
     expect(html).toMatch('Your Feed')
-    // expect(html).toMatch('Global Feed')
-    expect(html).toMatch('Global Feeds')    // should fail
 
     const href = await page.getAttribute('.navbar-brand', 'href')
-    expect(href).not.toMatch('https://react-redux.realworld.io')
-
-    await browser.close()
-})()
+    expect(href).toBe('/#/')
+  });
+});
